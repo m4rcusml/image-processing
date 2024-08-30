2016b56f76eb0a271dcdd8ac5733ad9b9fe0e386
fmarcus549 <fmarcus549@gmail.com>
2024-08-30 23:29:00 +0000

    inicio

--- App.tsx
+++ App.tsx
@@ -1,11 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { CameraView, CameraType, useCameraPermissions, CameraCapturedPicture } from 'expo-camera';
+import { useRef, useState } from 'react';
+import { Button, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const camera = useRef<CameraView>(null);
+  const [facing, setFacing] = useState<CameraType>('back');
+  const [permission, requestPermission] = useCameraPermissions();
+  const [image, setImage] = useState<CameraCapturedPicture | undefined>();
+  const [isTakingPicture, setIsTakingPicture] = useState(false);
+
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="Permitir uso da camera" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+
+  function takePicture() {
+    camera.current?.takePictureAsync().then(image => {
+      setImage(image);
+      setIsTakingPicture(false);
+    });
+  }
+
+  if (!isTakingPicture) {
+    return (
+      <View style={styles.container}>
+        <Image
+          style={{ height: 250, backgroundColor: '#999' }}
+          source={{ uri: image?.uri }}
+          resizeMode='contain'
+        />
+        <View>
+          <TouchableOpacity style={{ borderRadius: 10, borderWidth: 0.8, borderColor: 'black', padding: 10 }} onPress={() => setIsTakingPicture(true)}>
+            <Text>Take Picture</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <CameraView ref={camera} style={styles.camera} facing={facing} mode='picture'>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={takePicture}>
+            <Text style={styles.text}>Take Picture</Text>
+          </TouchableOpacity>
+        </View>
+      </CameraView>
     </View>
   );
 }
@@ -13,8 +71,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 30,
+    paddingHorizontal: 16,
+    gap: 20
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
